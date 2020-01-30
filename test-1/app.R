560f105d95162f4b647c43a67a6770591dce534a
AndersT123 <troldmanders@gmail.com>
2020-01-30 12:36:48 +0000

    added test-1 with library statements

---
+++ test-1/app.R
@@ -0,0 +1,48 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(rvest)
+library(shiny)
+library(shinydashboard)
+library(DT)
+library(shinyjs)
+library(sodium)
+library(tidyverse)
+library(rhandsontable)
+library(lubridate)
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           textOutput("wd")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$wd <- renderPrint({
+        out <- getwd()
+        out
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
