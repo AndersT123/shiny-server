296a4e03ce75e5f488e2c3de0a2e4d34bb1efdc6
AndersT123 <anderstvistholm@live.dk>
2020-02-03 22:16:57 +0100

    moved login page to modules

--- emtest/app.R
+++ emtest/app.R
@@ -11,73 +11,27 @@ source("./helper.R")
 source("./global.R")
 source("./modules/user_table.R")
 source("modules/games-table.R")
-# Main login screen
-
-loginpage <- div(id = "loginpage", style = "width: 500px; max-width: 100%; margin: 0 auto; padding: 20px;",
-                 wellPanel(
-                   tags$h2("LOG IN", class = "text-center", style = "padding-top: 0;color:#333; font-weight:600;"),
-                   textInput("user_name", placeholder="Username", label = tagList(icon("user"), "Username")),
-                   passwordInput("passwd", placeholder="Password", label = tagList(icon("unlock-alt"), "Password")),
-                   br(),
-                   div(
-                     style = "text-align: center;",
-                     actionButton("login", "SIGN IN", style = "color: white; background-color:#3c8dbc;
-                                  padding: 10px 15px; width: 150px; cursor: pointer;
-                                  font-size: 18px; font-weight: 600;"),
-                     shinyjs::hidden(
-                       div(id = "nomatch",
-                           tags$p("Oops! Incorrect username or password!",
-                                  style = "color: red; font-weight: 600; 
-                                  padding-top: 5px;font-size:16px;", 
-                                  class = "text-center")))
-                     ))
-                     )
-
-
+source("modules/login-page.R")
 
 header <- dashboardHeader( title = "Simple Dashboard", uiOutput("logoutbtn"))
-
 sidebar <- dashboardSidebar(uiOutput("sidebarpanel")) 
 body <- dashboardBody(shinyjs::useShinyjs(), uiOutput("body"))
+
 ui<-dashboardPage(header, sidebar, body, skin = "blue")
 
 server <- function(input, output, session) {
+  observeEvent(input$debug,{
+    browser()
+  })
   
+  login_mod <- callModule(loginpage, "login")
   callModule(user_table, "user_table")
   server_start <- function() now()
-  callModule(games_table, "games_table", reactive({input$user_name}) , server_start = server_start)
+  callModule(games_table, "games_table", reactive({login_mod$user()}) , server_start = server_start)
   
-  login = FALSE
-  USER <- reactiveValues(login = login)
-  
-  observe({
-    #browser()
-    if (USER$login == FALSE) {
-      if (!is.null(input$login)) {
-        if (input$login > 0) {
-          Username <- isolate(input$user_name)
-          Password <- isolate(input$passwd)
-          if(length(which(credentials$user_name==Username))==1) {
-            # Extract the encrypted password as character
-            pasmatch  <- credentials["user_pwpass"][which(credentials$user_name==Username),] %>% pull()
-            pasverify <- password_verify(pasmatch, Password)
-            if(pasverify) {
-              USER$login <- TRUE
-            } else {
-              shinyjs::toggle(id = "nomatch", anim = TRUE, time = 1, animType = "fade")
-              shinyjs::delay(3000, shinyjs::toggle(id = "nomatch", anim = TRUE, time = 1, animType = "fade"))
-            }
-          } else {
-            shinyjs::toggle(id = "nomatch", anim = TRUE, time = 1, animType = "fade")
-            shinyjs::delay(3000, shinyjs::toggle(id = "nomatch", anim = TRUE, time = 1, animType = "fade"))
-          }
-        } 
-      }
-    }    
-  })
 
   output$logoutbtn <- renderUI({
-    req(USER$login)
+    req(login_mod$login())
     tags$li(a(icon("fa fa-sign-out"), "Logout", 
               href="javascript:window.location.reload(true)"),
             class = "dropdown", 
@@ -86,20 +40,15 @@ server <- function(input, output, session) {
   })
   
   output$sidebarpanel <- renderUI({
-    if (USER$login == TRUE ){ 
+    if (login_mod$login() == TRUE){ 
       sidebarMenu(
         menuItem("Main Page", tabName = "dashboard", icon = icon("dashboard"))
       )
     }
   })
-  
-  observeEvent(input$debug,{
-    browser()
-  })
-  
-  
+
   output$body <- renderUI({
-    if (USER$login == TRUE ) {
+    if (login_mod$login() == TRUE) {
       tabItem(tabName ="dashboard", class = "active",
               fluidRow(
                 box(width = 9, 
@@ -110,8 +59,8 @@ server <- function(input, output, session) {
               
       )
     }
-    else {
-      loginpage
+    else { 
+      loginpage_ui("login")
     }
   })
   
